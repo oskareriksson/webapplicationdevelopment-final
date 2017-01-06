96d720d9249250a0e309b9a0dd85c0c8f560276d
Oskar Eriksson <oskar.eriksson.96@hotmail.com>
2017-01-06 16:46:34 +0100

    Added some changes to index and script file

--- js/myScript.js
+++ js/myScript.js
@@ -1,14 +1,33 @@
 $(document).ready(function() {
-	$("button").click(function() {
-		var $userInput = $("input").val();
+
+	//GET request for Spotify-artist using "search for an item" method
+	$("#b1").click(function() {
+		var $artistInput = $("#i1").val();
 
 		$.ajax({
 			type: "GET",
-			url: "https://api.spotify.com/v1/search?q=" + $userInput + "&type=artist",
+			url: "https://api.spotify.com/v1/search?q=" + $artistInput + "&type=artist&limit=5",
 			success: function(data) {
 				console.log(data.artists.items[0].name);
 				$("ul").append("<li>" + data.artists.items[0].name + "</li>");
 				$("body").append("<img src=" + data.artists.items[0].images[2].url + ">");
+				$("#i1").val("");
+			}
+		});
+	});
+
+	//GET request for Spotify-track using "search for an item" method
+	$("#b2").click(function() {
+		var $trackInput = $("#i2").val();
+
+		$.ajax({
+			type: "GET",
+			url: "https://api.spotify.com/v1/search?q=" + $trackInput + "&type=track&limit=5",
+			success: function(data) {
+				console.log(data.tracks.items[0].name);
+				$("ul").append("<li>" + data.tracks.items[0].name + "</li>");
+				
+				$("#i2").val("");
 			}
 		});
 	});
