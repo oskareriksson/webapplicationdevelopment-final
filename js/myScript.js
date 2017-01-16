a04631c750a7adb83ff2f1ee8573a28d809e2dbd
Oskar Eriksson <oskar.eriksson.96@hotmail.com>
2017-01-16 19:09:31 +0100

    Finished fetchprofile function for now, will prob tweak it again later though

--- js/myScript.js
+++ js/myScript.js
@@ -24,10 +24,6 @@ var fetchprofile = function() {
 			url: "https://api.lootbox.eu/" + $platform + "/" + $region + "/" + $btag + "/profile",
 			dataType: "json",
 			success: function(d){
-				/*console.log(d.data.username);
-				console.log(d.data.level);
-				console.log(d.data.games.quick.wins);
-				console.log(d.data.games.competitive.wins);*/
 				$mainrow.empty();
 				var $profileImg = d.data.avatar;
 				var $username = d.data.username;
@@ -41,9 +37,31 @@ var fetchprofile = function() {
 				var $compRank = d.data.competitive.rank;
 				var $compRankImg = d.data.competitive.rank_img;
 
+				if($compWins === undefined || null){
+					var $compWins = "-";
+				}
+				if($compLoss === null || undefined){
+					var $compLoss = "-";
+				}
+				if($compRank === null || undefined){
+					var $compRank = "-";
+				}
+
 				$("<img>").attr("src", $profileImg).appendTo($mainrow);
-				$("<h3>" + $username + "</h3>").addClass("center").appendTo($mainrow);
-				$("<h4>Level: " + $level + "</h4>").addClass("center").appendTo($mainrow);
+				$("<h2>" + $username + "</h2>").addClass("center").appendTo($mainrow);
+				$("<h3>Level: " + $level + "</h3>").addClass("center").appendTo($mainrow);
+				$("<h3>Playtime</h3>").addClass("center").appendTo($mainrow);
+				$("<h4>Quickplay: " + $quickPlaytime + "</h4>").addClass("col-xs-6 center").appendTo($mainrow);
+				$("<h4>Competitive: " + $compPlaytime + "</h4>").addClass("col-xs-6 center").appendTo($mainrow);
+				$("<h3>Wins/Losses</h3>").addClass("center").appendTo($mainrow);
+				$("<h3>Quickplay</h3>").addClass("center").appendTo($mainrow);
+				$("<h4>" + $quickWins + " Won</h4>").addClass("center").appendTo($mainrow);
+				$("<h3>Competitive</h3>").addClass("center").appendTo($mainrow);
+				$("<h4>" + $compWins + " Won</h4>").addClass("center").appendTo($mainrow);
+				$("<h4>" + $compLoss + " Lost</h4>").addClass("center").appendTo($mainrow);
+				$("<h4>" + $compPlayed + " Played</h4>").addClass("center").appendTo($mainrow);
+				$("<img>").attr("src", $compRankImg).appendTo($mainrow);
+				$("<h3>" + $compRank + " Rating</h3>").addClass("center").appendTo($mainrow);
 			},
 			error: function(jqXHR, textStatus, errorThrown){
 				console.log(textStatus, errorThrown);
