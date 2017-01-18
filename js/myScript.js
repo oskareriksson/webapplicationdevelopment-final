8fa578f9fdf979f09a1440bea801c530bddb0473
Oskar Eriksson <oskar.eriksson.96@hotmail.com>
2017-01-18 14:12:23 +0100

    Made fetchplaytime function much more practical using a for loop

--- js/myScript.js
+++ js/myScript.js
@@ -176,7 +176,7 @@ var fetchplaytime = function() {
 			dataType: "json",
 			success: function(d) {
 				$mainrow.empty();
-				$("<img>").attr("src", d[0].image).appendTo($mainrow);
+				/*$("<img>").attr("src", d[0].image).appendTo($mainrow);
 				$("<h3>" + d[0].name + "</h3>").addClass("center").appendTo($mainrow);
 				$("<h3>" + d[0].playtime + "</h3>").addClass("center").appendTo($mainrow);
 				$("<img>").attr("src", d[1].image).appendTo($mainrow);
@@ -188,6 +188,14 @@ var fetchplaytime = function() {
 				$("<img>").attr("src", d[3].image).appendTo($mainrow);
 				$("<h3>" + d[3].name + "</h3>").addClass("center").appendTo($mainrow);
 				$("<h3>" + d[3].playtime + "</h3>").addClass("center").appendTo($mainrow);
+				$("<img>").attr("src", d[4].image).appendTo($mainrow);
+				$("<h3>" + d[4].name + "</h3>").addClass("center").appendTo($mainrow);
+				$("<h3>" + d[4].playtime + "</h3>").addClass("center").appendTo($mainrow);*/
+				for(i = 0; i < d.length; i++){
+					$("<img>").attr("src", d[i].image).appendTo($mainrow);
+					$("<h3>" + d[i].name + "</h3>").addClass("center").appendTo($mainrow);
+					$("<h3>" + d[i].playtime + "</h3>").addClass("center").appendTo($mainrow);
+				}
 			},
 			error: function(jqXHR, textStatus, errorThrown){
 				console.log(textStatus, errorThrown);
